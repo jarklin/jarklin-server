542aefaf6de9989bb7073ac31472c1b38256d5ca
PlayerG9 <g9yout@gmail.com>
2024-01-08 10:01:18 +0100

    fixed startup script fallback-python3 detection

--- jarklin.sh
+++ jarklin.sh
@@ -6,7 +6,7 @@ THIS=$(dirname "$(realpath "$0")")
 PYTHON3="$THIS/.venv/bin/python3"
 
 if [ ! -f "$PYTHON3" ]; then
-  PYTHON3=$(/usr/bin/env python3)
+  PYTHON3=$(which python3)
 fi
 
 PYTHONPATH="$PYTHONPATH:$THIS/src/" "$PYTHON3" -X utf8 -BO -m jarklin "$@"
