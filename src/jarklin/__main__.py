a86aab3ac3100e2f8f8a86dfe01f308a6b8a83d3
PlayerG9 <g9yout@gmail.com>
2024-02-03 11:20:47 +0100

    fixed problem of not starting because of --verify-jarklin

--- src/jarklin/__main__.py
+++ src/jarklin/__main__.py
@@ -15,9 +15,14 @@ except ModuleNotFoundError:
 
 
 class VerifyJarklinAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
+    # noinspection PyShadowingBuiltins
+    def __init__(self, option_strings, dest=ap.SUPPRESS, default=ap.SUPPRESS,
+                 help="helps to verify that this executable is jarklin"):
+        super().__init__(option_strings=option_strings, dest=dest, default=default, nargs=0, help=help)
+
+    def __call__(self, parser_, namespace, values, option_string=None):
         sys.stdout.write("jarklin")
-        parser.exit()
+        parser_.exit()
 
 
 # ==================================================================================================================== #
