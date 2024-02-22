bf2da92dcb401a8b2effcfa4ef84638db2304904
PlayerG9 <g9yout@gmail.com>
2024-02-22 08:52:32 +0100

    extended `jarklin run` with logging

--- src/jarklin/_commands/run/__init__.py
+++ src/jarklin/_commands/run/__init__.py
@@ -6,6 +6,7 @@ r"""
 
 def run():
     import time
+    import logging
     import multiprocessing
     from ..web import run as web_run
     from ..cache import run as cache_run
@@ -20,9 +21,17 @@ def run():
         while web.is_alive() and cache.is_alive():
             time.sleep(1)
     except KeyboardInterrupt:
+        logging.info("Caught KeyboardInterrupt. Shutting down...")
         if web.is_alive():
             web.terminate()
         if cache.is_alive():
             cache.terminate()
         web.join(timeout=10)
         cache.join(timeout=10)
+    else:
+        if web.is_alive():  # web alive, cache crashed
+            web.terminate()
+            logging.critical("jarklin-cache crashed")
+        if cache.is_alive():  # cache alive, web crashed
+            cache.terminate()
+            logging.critical("jarklin-web crashed")
