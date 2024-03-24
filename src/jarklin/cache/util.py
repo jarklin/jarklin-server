2ff43cb325dc4e7f695573cf25c5cf0237875f24
playerg9 <g9yout@gmail.com>
2024-03-24 17:07:41 +0100

    fixed creation time for linux

--- src/jarklin/cache/util.py
+++ src/jarklin/cache/util.py
@@ -7,6 +7,10 @@ import mimetypes
 import os.path as p
 from pathlib import Path
 from ..common.types import PathSource
+try:
+    import statx
+except ModuleNotFoundError:
+    statx = None
 
 
 any_number = re.compile(r"\d")
@@ -57,35 +61,46 @@ def is_deprecated(source: PathSource, dest: PathSource) -> bool:
         raise FileNotFoundError(source)
     if not dest.exists():
         return True
-    if source.is_dir():  # gallery
-        times = [int(p.getctime(fp)) for fp in source.iterdir() if fp.is_file()]
-        source_mtime = max(times) if times else p.getmtime(source)
-    else:
-        source_mtime = p.getmtime(source)
-    times = [p.getmtime(fp) for fp in dest.iterdir() if fp.is_file()]
-    dest_mtime = max(times) if times else p.getmtime(dest)
+    source_mtime = get_modification_time(source)
+    dest_mtime = get_modification_time(dest)
     return source_mtime > dest_mtime
 
 
+def _get_creation_time(fp: PathSource) -> float:
+    r"""
+    @linux ctime => changed-time, btime => birth-time
+    @windows ctime => creation-time
+    """
+    path = Path(fp)
+    try:
+        if statx is None:
+            raise RuntimeError()
+        return statx.statx(str(fp)).btime
+    except RuntimeError:
+        return path.stat().st_ctime
+
+
 def get_creation_time(path: PathSource) -> float:
-    # fixme: ctime != creation-time on unix
+    r"""
+    returns smallest creation time
+    if directory returns earliest from files
+    """
     path = Path(path)
     if path.is_dir():
-        times = [int(p.getctime(fp)) for fp in path.iterdir() if fp.is_file()]
-        return min(times) if times else p.getctime(path)
+        times = [int(_get_creation_time(fp)) for fp in path.iterdir() if fp.is_file()]
+        return min(times) if times else _get_creation_time(path)
     else:
-        return int(p.getctime(path))
+        return int(_get_creation_time(path))
 
 
 def get_modification_time(path: PathSource) -> float:
+    r"""
+    returns modification time
+    if directory returns biggest from files
+    """
     path = Path(path)
     if path.is_dir():
         times = [int(p.getmtime(fp)) for fp in path.iterdir() if fp.is_file()]
-        if not times:  # no files in directory
-            return p.getmtime(path)
-        minimum = min(times)
-        maximum = max(times)
-        # assume that it took at least one hour for the gallery to be created (e.g. download-time)
-        return maximum if maximum > (minimum + 3600) else minimum
+        return max(times) if times else p.getmtime(path)
     else:
         return int(p.getmtime(path))
