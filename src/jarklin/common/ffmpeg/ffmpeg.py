2723648b3101ab68f68bc279b1725fe3cdcf523e
PlayerG9 <g9yout@gmail.com>
2024-04-04 11:44:52 +0200

    added logging to common.ffmpeg

--- src/jarklin/common/ffmpeg/ffmpeg.py
+++ src/jarklin/common/ffmpeg/ffmpeg.py
@@ -2,14 +2,21 @@
 r"""
 
 """
+import shlex
+import logging
 import typing as t
 import subprocess as sp
 from configlib import config
+
+
+logger = logging.getLogger(__name__)
 
 
 def ffmpeg(args: t.Iterable[str]):
     ffmpeg_executable = config.getstr('ffmpeg', fallback="ffmpeg")
 
     args = [ffmpeg_executable, '-hide_banner', *args]
+
+    logger.debug(f"running: {shlex.join(args)}")
 
     return sp.run(args, check=True, capture_output=True, text=True)
--- src/jarklin/common/ffmpeg/ffprobe.py
+++ src/jarklin/common/ffmpeg/ffprobe.py
@@ -2,6 +2,9 @@
 r"""
 
 """
+import json
+import shlex
+import logging
 import typing as t
 from os import PathLike
 import subprocess as sp
@@ -9,15 +12,20 @@ from configlib import config
 from .ffprope_typing import FFProbeResult
 
 
+logger = logging.getLogger(__name__)
+
+
 def ffprobe(fp: t.Union[str, PathLike]) -> FFProbeResult:
-    import json
 
     ffprobe_executable = config.getstr('ffprobe', fallback="ffprobe")
 
-    result = sp.run([
+    args = [
         ffprobe_executable, '-hide_banner',
         '-show_format', '-show_streams', '-show_chapters', '-count_frames',
         '-of', 'json', str(fp),
-    ], check=True, capture_output=True, text=True,
-    )
+    ]
+
+    logger.debug(f"running: {shlex.join(args)}")
+
+    result = sp.run(args, check=True, capture_output=True, text=True)
     return json.loads(result.stdout)
